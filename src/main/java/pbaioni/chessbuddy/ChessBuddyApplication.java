2a64939d1e2dc1cba9b944db390af3388d610f9c
pbaioni <baioni2002@yahoo.fr>
2023-01-19 13:55:40 +0100

    setup project

---
+++ src/main/java/pbaioni/chessbuddy/ChessBuddyApplication.java
@@ -0,0 +1,13 @@
+package pbaioni.chessbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pbaioni/chessbuddy/ChessBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package pbaioni.chessbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
