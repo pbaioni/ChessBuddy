eb387c49ee75f8cdacc7a2d34fc362ef594ef52a
pbaioni <baioni2002@yahoo.fr>
2023-01-19 14:54:58 +0100

    first commit

--- src/main/java/pbaioni/chessbuddy/ChessBuddyApplication.java
+++ src/main/java/pbaioni/chessbuddy/ChessBuddyApplication.java
@@ -3,11 +3,19 @@ package pbaioni.chessbuddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import stockfish4j.service.Stockfish4jProperties;
+import stockfish4j.service.StockfishService;
+
 @SpringBootApplication
 public class ChessBuddyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChessBuddyApplication.class, args);
+		System.out.println("started");
+		Stockfish4jProperties props = Stockfish4jProperties.getInstance();
+		System.out.println(props.getStockfishInstances());
+		StockfishService service = new StockfishService();
+		service.submitDepthTask("3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1", 16);
 	}
 
 }
